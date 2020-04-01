c7f01273dc9e4524250c708bcf795afb8507d972
Legokiller <commanderpip@gmail.com>
2020-04-01 13:25:57 -0400

    Update with placeholder and pseudocode

--- src/internship/project1/FXMLDocumentController.java
+++ src/internship/project1/FXMLDocumentController.java
@@ -7,10 +7,13 @@ package internship.project1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import static javafx.application.Platform.exit;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -22,9 +25,34 @@ public class FXMLDocumentController implements Initializable {
     private Label label;
     
     @FXML
+    private Button closeButton;
+    
+    @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World! Test!");
+    }
+    
+    @FXML
+    private void openButtonAction(ActionEvent event)
+    {
+        
+    }
+    
+    @FXML
+    private void saveButtonAction(ActionEvent event)
+    {
+        //Runs the RenameSTD X function
+        //RunRename.RenameSTD();
+        //Once it is saved...
+        label.setText("Your new file name is 4/1/JWheel.std");
+        
+    }
+    
+    @FXML
+    private void closeButtonAction(ActionEvent event)
+    {
+        exit();
     }
     
     @Override
